3068b3e1c0f2e17b5afe7fbeef134b53f8f4d1fd
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-03-28 16:13:20 +0000

    Se agrega IMC

--- app/src/main/java/com/example/mobile1project/MainActivity.kt
+++ app/src/main/java/com/example/mobile1project/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.mobile1project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.mobile1project.sum.views.SumView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mobile1project.imc.views.IMCScreen
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // 🔥 Se agregó el parámetro `savedInstanceState`
         super.onCreate(savedInstanceState)
         setContent {
-            SumView()
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) { // 🔥 Se limpió la importación del `Modifier`
+                    IMCScreen()
+                }
+            }
         }
     }
 }
-
 
---
+++ app/src/main/java/com/example/mobile1project/imc/models/ImcModel.kt
@@ -0,0 +1,1 @@
+package com.example.mobile1project.imc.models
